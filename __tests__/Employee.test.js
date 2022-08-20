268154a4cde383473188ea31793bf8a4b5d52be7
Mike Rugh <mike.rugh@gmail.com>
2022-08-20 14:35:22 -0700

    adds employee tests and class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,19 @@
-const Employee = require('./lib/employee.js');
+const Employee = require('../lib/Employee');
 
+describe('getName', () => {
+    it('Employee name should be returned', () => {
+      expect(new Employee('Bob', '77', 'bob@bob.com').getName()).toBe('Bob');
+    });
+
+    it('Employee ID should be returned', () => {
+        expect(new Employee('Bob', '77', 'bob@bob.com').getId()).toBe('77');
+    });
+
+    it('Employee email should be returned', () => {
+        expect(new Employee('Bob', '77', 'bob@bob.com').getEmail()).toBe('bob@bob.com');
+    });
+
+    it('Role should be returned', () => {
+        expect(new Employee('Bob', '77', 'bob@bob.com').getRole()).toBe('Employee');
+    });
+});
