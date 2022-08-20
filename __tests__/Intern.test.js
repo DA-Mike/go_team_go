4945bf4aa40295af7314caf0bcbf78602c5e5324
Mike Rugh <mike.rugh@gmail.com>
2022-08-20 15:07:25 -0700

    adds intern test and class

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,23 @@
+const Intern = require('../lib/Intern');
+
+describe('Intern', () => {
+    it('Employee name should be returned', () => {
+      expect(new Intern('Bob', '77', 'bob@bob.com', 'Cal').getName()).toBe('Bob');
+    });
+
+    it('Employee name should be returned', () => {
+        expect(new Intern('Bob', '77', 'bob@bob.com', 'Cal').getId()).toBe('77');
+      });
+
+    it('Employee email should be returned', () => {
+        expect(new Intern('Bob', '77', 'bob@bob.com', 'Cal').getEmail()).toBe('bob@bob.com');
+    });
+
+    it('Role should be returned', () => {
+        expect(new Intern('Bob', '77', 'bob@bob.com', 'Cal').getRole()).toBe('Intern');
+    });
+
+    it('Role should be returned', () => {
+        expect(new Intern('Bob', '77', 'bob@bob.com', 'Cal').getSchool()).toBe('Cal');
+    });
+});
