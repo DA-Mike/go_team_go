752ed278fbb45e5ad8a352e14499c260cabe3e98
Mike Rugh <mike.rugh@gmail.com>
2022-08-20 21:12:39 -0700

    adds styling

--- src/template.js
+++ src/template.js
@@ -2,9 +2,17 @@ const fs = require('fs');
 
 function generateHTML(data) {
         fs.writeFile('./dist/index.html', generateBody(data), (err) =>
-        err ? console.error(err) : console.log('Success!'));
+        err ? console.error(err) : console.log('Success!'))
         // addCards(data);
+        // .then(() => addCards(data));
 }
+
+// const generateHTML = (data) => 
+//     fs.writeFile('./dist/index.html', generateBody(data), (err) =>
+//     err ? console.error(err) : console.log('Success!'))
+//     .then(() => addCards(data));
+
+
 
 function generateBody(data) {
     // console.log("generateBody: ", data);
@@ -15,11 +23,12 @@ function generateBody(data) {
                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+                    <link href="style.css" rel="stylesheet">
                     <title>Go Team Go!</title>
                 </head>
                 <body>
-                <header>
-                    <h1 class="my_team danger">My Team</h1>
+                <header class="my_team_header bg-danger">
+                    <h1 class="my_team text_white">My Team</h1>
                 </header>
                 <div class="container team_container" style="background-color: white";>
                 ${addCards(data)}
@@ -33,42 +42,34 @@ function generateBody(data) {
 
 function addCards(data) {
     console.log("addCards: ", data);
-    const domArr = [];
+    var domStr = '';
     for (i = 0; i < data.length; i++) {
-        let domStr = 
-         `
-        <div class="card_container">
-            <div class="banner_container">
-            <h2>${data[i].name}</h2>
-            <h3>${data[i].role}</h3> 
+    let newEl = `
+        <div class="card_container bg-light">
+            <div class="banner_container bg-primary">
+            <h2 class="text-white">${data[i].name}</h2>
+            <h3 class="text-white">${data[i].role}</h3> 
             </div>
+            <div class="bg-light">
             <ul class="info_list">
-            <li>ID: ${data[i].id}</li>
-            <li>Email: ${data[i].email}</li>
-            <li>${lastElement(data[i])}</li>
+            <li class="list_item">ID: ${data[i].id}</li>
+            <li class="list_item">Email: <a href="mailto:${data[i].email}">${data[i].email}</a></li>
+            <li class="list_item">${lastElement(data[i])}</li>
             </ul>
+            </div>
         </div>
         `
-        domArr.push(domStr);
+        domStr = domStr.concat(newEl);
     }
-    
-    // function sendToDom(data) {
-    //     console.log("sendToDom: ", data);
-    //     return data;
-    // }
-
-    // return domArr.forEach(sendToDom);
-    const iterator = domArr.values();
-    for (const value of iterator) {
-        return value;
-      }
+    console.log('domStr: ', domStr);
+    return domStr;
 }
 
 function lastElement(data) {
     if (data.role === 'Manager') {
         return `Office Number: ${data.officeNumber}`;
     } else if (data.role === 'Engineer') {
-        return `GitHub: ${data.github}`;
+        return `GitHub: <a href="https://github.com/${data.github}/" target="_blank">${data.github}</a>`;
     } else {
         return `School: ${data.school}`;
     }
@@ -76,41 +77,3 @@ function lastElement(data) {
 
 module.exports = generateHTML;
 
-// function addCards(data) {
-//     console.log("addCards:", data);
-
-//     for (i = 0; i < data.length; i++) {
-    
-//         const teamEl = document.getElementsByClassName('team_container');
-//         const cardEl = document.createElement('div');
-//         const bannerEl = document.createElement('div');
-//         const nameEl = document.createElement('h2');
-//         const roleEl = document.createElement('h3');
-//         const listEl = document.createElement('ul');
-//         const idEl = document.createElement('li');
-//         const emailEl = document.createElement('li');
-//         const varEl = document.createElement('li');
-
-//         nameEl.innerHTML = data[i].name;
-//         roleEl.innerHTML = data[i].role;
-//         idEl.innerHTML = data[i].id;
-//         emailEl.innerHTML = data[i].email;
-        
-//         if (data[i].role === 'Manager') {
-//             varEl.innerHTML = data[i].officeNumber;
-//         } else if (data[i].role === 'Engineer') {
-//             varEl.innerHTML = data[i].github;
-//         } else {
-//             varEl.innerHTML = data[i].school;
-//         }
-
-//         bannerEl.appendChild(nameEl);
-//         bannerEl.appendChild(roleEl);
-//         cardEl.appendChild(bannerEl);
-//         listEl.appendChild(idEl);
-//         listEl.appendChild(emailEl);
-//         listEl.appendChild(varEl);
-//         cardEl.appendChild(listEl);
-//         teamEl.appendChild(cardEl);
-//     }
-// }
